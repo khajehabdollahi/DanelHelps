504d89c548c7ad55237a4a8c3966d9127dc73943
Hassan <pkhajeh@gmail.com>
2019-10-21 12:12:49 +0200

    yu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
+        System.out.println("Yes");
     }
 }
